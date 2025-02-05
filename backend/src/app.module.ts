23b061933f2cd07ebd26184ec13583c780e0bd45
Livio Theiler <livio.theiler@jmcsoftwareag.onmicrosoft.com>
2025-02-05 11:45:04 +0100

    feat: add mongodb and dockerize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { MongooseModule } from '@nestjs/mongoose'
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ 
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true 
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'global',
+          limit: 10,
+          ttl: 60
+        }
+      ]
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI ?? 'mongodb://localhost:27017', {
+      user: process.env.DATABASE_USER,
+      pass: process.env.DATABASE_PASS,
+      dbName: process.env.DATABASE_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common'
+import helmet from 'helmet'
+import * as compression from 'compression'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({ origin: '*' });
+  app.useGlobalPipes(new ValidationPipe({}));
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
+  app.use(helmet());
+  app.use(compression());
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT ?? 3000}`);
+  });
 }
 bootstrap();
